49077485f4d666c1f17e33738b6c64ef97387a3f
Ryan Brink <rbweb@pm.me>
2022-05-05 12:49:24 -0700

    crud router

--- src/api/main.py
+++ src/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi_crudrouter import MemoryCRUDRouter as CRUDRouter
 
 from src.persistence.database import initialize_database
+from src.persistence.models import Author, Book
 
 app = FastAPI()
 db = initialize_database()
@@ -9,3 +11,7 @@ db = initialize_database()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+app.include_router(CRUDRouter(schema=Author))
+app.include_router(CRUDRouter(schema=Book))
